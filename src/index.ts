da65e64d44c993ec3629e02d7889f41ec70032aa
i-do-dev <i.do.dev@outlook.com>
2021-12-08 11:32:48 +0500

    draw a circle

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
-const sayHello = () => {
-    let messageInput: HTMLInputElement = <HTMLInputElement>document.getElementById('message');
-    let message: String = messageInput.value;
-    type WindowStates = "open" | "closed" | "minimized";
-    let windowState: WindowStates = "open";
-    console.log("windowState: ", windowState);
-    console.log("Message: " + message);
-    
-}
+import Two from 'two.js';;
 
 const init = () => {
-    let sayItInput: HTMLInputElement = <HTMLInputElement>document.getElementById('say');
-    sayItInput.addEventListener('click', sayHello);
+    console.log("two ==000== ", Two);
+
+    var params = {fullscreen: true};
+    var elem = document.body;
+    var two = new Two(params).appendTo(elem);
+    
+    // Two.js has convenient methods to make shapes and insert them into the scene.
+    var radius = 50;
+    var x = two.width * 0.5;
+    var y = two.height * 0.5 - radius;
+    //var y = two.height * 0.5 - radius * 1.25;
+    var circle = two.makeCircle(x, y, radius);
+    
+    // The object returned has many stylable properties:
+    circle.fill = '#FF8000';
+    // And accepts all valid CSS color:
+    circle.stroke = 'blue';
+    circle.linewidth = 5;
+
+    two.update();
+    
 }
 
 document.addEventListener('DOMContentLoaded', () => {
