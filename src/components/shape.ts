466ab100843f897343a0afd15d37cc82e74ee8d7
i-do-dev <i.do.dev@outlook.com>
2021-12-10 23:30:46 +0500

    abstract shap

---
+++ src/components/shape.ts
@@ -0,0 +1,38 @@
+import Two from 'two.js';
+import {IVector} from '../interface';
+
+export default abstract class Shape {
+    private _position: IVector;
+    private _shape: any;
+    private _skin: any;
+    
+    constructor(shape: any = null, params: any) {
+        this.shape = shape;
+        this.skin = params.skin;
+        let x = 100;
+        let y = 100;
+        this.position = {x, y};
+    }
+    
+    public get position(): IVector {
+        return this._position
+    };
+
+    public set position(value: IVector) {
+        if (this.shape) {
+            this.shape.position.set(value.x, value.y);
+        }
+        this._position = value; 
+    }
+
+    public set skin(value: any) { 
+        if (this.shape) {
+            this.shape.noStroke().fill = value.color;
+        }
+        this._skin = value; 
+    }
+    public get skin() { return this._skin; }
+    
+    public get shape(): any { return this._shape; };
+    public set shape(value: any) { this._shape = value;}
+}
