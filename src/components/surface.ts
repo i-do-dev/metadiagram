b05c743eb53e2de4b8bd4ce23f7c30e5a7f79f24
i-do-dev <i.do.dev@outlook.com>
2021-12-11 12:01:14 +0500

    surface component :zap:

--- src/components/surface.ts
+++ src/components/surface.ts
@@ -6,26 +6,25 @@ export default class Surface {
     private _zui: any;
     private _mouse: any;
     private _dragging: boolean = false;
-
+    private _mouseDownHandler: any;
+    private _mouseMoveHandler: any;
+    private _mouseUpHandler: any;
+    
     constructor(el: any, shape: any, group: any) {
-        require('two.js/extras/js/zui') ;
-        this.zui = new Two.ZUI(group);
-        this.zui.addLimits(0.1, 8);
+        let self = this;
+        this._zui = new Two.ZUI(group);
+        this._zui.addLimits(0.1, 8);
         this.mouse = new Two.Vector();
-        this.el = el;
-        this.shape = shape;
-        this.el.addEventListener('mousedown', this.mousedown.bind(this), false);
+        this._el = el;
+        this._shape = shape;
+        this._mouseDownHandler = (e: MouseEvent) => { self.mousedown(e); }
+        this.el.addEventListener('mousedown', this._mouseDownHandler , false);
+        
     }
 
-    public set el(el: any) { this._el = el; }
     public get el(): any { return this._el; }
-
-    public set shape(shape: any) { this._shape = shape; };
     public get shape(): any { return this._shape; }
 
-    public set zui(zui: any) { this._zui = zui};
-    public get zui(): any { return this._zui; };
-    
     public set mouse(mouse: any) { this._mouse = mouse};
     public get mouse(): any { return this._mouse; };
     
@@ -33,34 +32,38 @@ export default class Surface {
     public get dragging(): any { return this._dragging; };
 
     
+    
     public mousedown(e: MouseEvent): void {
+        let self = this;
         this.mouse.x = e.clientX;
         this.mouse.y = e.clientY;
         let rect = this.shape.getBoundingClientRect();
         this.dragging = this.mouse.x > rect.left && this.mouse.x < rect.right
         && this.mouse.y > rect.top && this.mouse.y < rect.bottom;
-        window.addEventListener('mousemove', this.mousemove.bind(this), false);
-        window.addEventListener('mouseup', this.mouseup.bind(this), false);
+
+        this._mouseMoveHandler = (e: MouseEvent) => { self.mousemove(e); }
+        this._mouseUpHandler = (e: MouseEvent) => { self.mouseup(e); }
+        window.addEventListener('mousemove', this._mouseMoveHandler , false);
+        window.addEventListener('mouseup', this._mouseUpHandler, false);
     }
 
     public mousemove(e: MouseEvent): void {
         let dx = e.clientX - this.mouse.x, dy = e.clientY - this.mouse.y;
         if (this.dragging) {
-            this.shape.position.x += dx / this.zui.scale;
-            this.shape.position.y += dy / this.zui.scale;
+            this.shape.position.x += dx / this._zui.scale;
+            this.shape.position.y += dy / this._zui.scale;
         } else {
-            this.zui.translateSurface(dx, dy);
+            this._zui.translateSurface(dx, dy);
         }
 
         this.mouse.set(e.clientX, e.clientY);
     }
 
     public mouseup(e: MouseEvent) {
-        window.removeEventListener('mousemove',this.mousemove.bind(this), false);
-        window.removeEventListener('mouseup', this.mouseup.bind(this), false);
+        let self = this;
+        window.removeEventListener('mousemove', self._mouseMoveHandler , false);
+        window.removeEventListener('mouseup',  self._mouseUpHandler , false);
     }
-
-
 
     static create(el: any, shape: any, group: any): any {
         return new Surface(el, shape, group);
