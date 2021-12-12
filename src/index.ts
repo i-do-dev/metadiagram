ca25455d6a77dfdf2b3c42ceb21c7908b933258f
i-do-dev <i.do.dev@outlook.com>
2021-12-13 01:20:57 +0500

    adding multiple shapes :art:

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,46 @@ const init = () => {
     var params = {fullscreen: true};
     var elem = document.body;
     var two = new Two(params).appendTo(elem);
-    
+    var domElement = two.renderer.domElement;
+
+    // surface setup start
     var group = new Two.Group();
-    
+    var surface = Surface.create(domElement, group);
+
+    // Make shape 1
     let rectangleParams = {two, size: 100, skin: {
         color: 'brown'
     }};
-    
     var rectangle = Rectangle.create(rectangleParams);
     var shape = rectangle.shape;
+    surface.addShape(shape);
     
-    group.add(shape);
+    // Make shape 2
+    let rectangleParams1 = {two, size: 100, skin: {
+        color: 'green'
+    }};
+    var rectangle1 = Rectangle.create(rectangleParams1);
+    var shape1 = rectangle1.shape;
+    surface.addShape(shape1);
     
+    // surface setup end
     two.add(group);
-
-    var domElement = two.renderer.domElement;
-    Surface.create(domElement, shape, group);
-    
     two.play();
     // two.update();
+
+    var shapEventInitialzed = false;
+
+    two.bind("resize", function (e: Event) {
+        /* console.log("RESIZE", e); */        
+        
+    }).bind("update", function (frameCount: any, timeDelta: any) {
+        
+        if (!shapEventInitialzed) {
+            surface.initializeShapEvents();
+            shapEventInitialzed = true;
+        }
+        
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
